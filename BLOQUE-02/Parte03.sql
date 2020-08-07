5ce2a34986fba9b05aa38f3ad5e03e08451f69c7
gmanriquev <gmanrique.422@gmail.com>
2020-08-06 19:38:58 -0500

    Bloque 02 parte 03

--- BLOQUE-02/Parte03.sql
+++ BLOQUE-02/Parte03.sql
@@ -40,7 +40,6 @@ codzona de menor a mayor.*/
 NOT (estado=1 and codubigeo=1) order by codzona asc
 
 --02.08
-
 select 
 tipo,
 case when codtipo=3 then 'RUC' else 'OTRO TIPO' end as TIPO_DOC,
@@ -49,8 +48,35 @@ razon_social as RAZON_SOCIAL,
 codzona as CODZONA,
 fec_inicio as FEC_INICIO
 from Cliente
+/*a. Tipo_cliente=’E’ [Y] codzona de valor 1,3,5 o 7 ordenados alfabéticamente
+Z-A por razón_social*/
 --where tipo='E' and (codzona=1 or codzona=3 or codzona=5 or codzona=7)
-where tipo='E' and codzona in (1,3,5,7)
-order by razon_social desc
+--where tipo='E' and codzona in (1,3,5,7)
+--order by razon_social desc
+/*Tipo_cliente=’E’ [Y] fec_inicio desde el ‘01/01/1998’ al ‘31/12/1998’
+ordenados por fec_inicio del más reciente al más antiguo.*/
+where tipo='E' and fec_inicio between '1998-01-01' and '1998-12-31'
+order by fec_inicio desc
 
-
+--02.10
+select case when codtipo=1 then 'LE o DNI' else 'OTRO TIPO' end as TIPO_DOC,
+numdoc as NUM_DOC,
+nombres+' '+ape_paterno+' '+ape_materno as CLIENTE
+from Cliente
+where tipo='P'
+--a. Nombre completo inicie en ‘A’ ('ANA JULIA MORALES','AMADOR MANRIQUE RUIZ')
+--and nombres+' '+ape_paterno+' '+ape_materno like 'A%' --%:No importa en valor ni en longitud
+--b. Nombre completo contiene la secuencia ‘AMA’ ('AMADOR MANRIQUE RUIZ','JUAN AMARANTO LOPEZ')
+--and nombres+' '+ape_paterno+' '+ape_materno like '%AMA%'
+--c. Nombre completo finaliza en 'AN'.
+--and nombres+' '+ape_paterno+' '+ape_materno like '%AN'
+--e. Nombre completo contenga la secuencia ‘ARI’ desde la 2° posición ('MARIANA LOPEZ CABELLO')
+--and nombres+' '+ape_paterno+' '+ape_materno like '_ARI%' -- '_' No importa el valor pero longitud=cantidad '_'
+--f.Nombre completo tenga como antepenúltimo carácter la ‘M’ ('NNNNNNNM__')
+--and nombres+' '+ape_paterno+' '+ape_materno like '%M__'
+--h.Nombre completo inicie y finalice con una vocal. --[aeiou]:a,e, i,o,u.
+--and nombres+' '+ape_paterno+' '+ape_materno like '[aeiou]%[aeiou]'
+--i.Nombre completo inicie y finalice con una consonante. NOTA: Informacíón SOLO debe contener consonantes y vocales --[^aeiou]=Consonante
+--and nombres+' '+ape_paterno+' '+ape_materno like '[^aeiou]%[^aeiou]'
+--j.Nombre inicie con una vocal y finalice con una consonante.
+and nombres+' '+ape_paterno+' '+ape_materno like '[aeiou]%[^aeiou]'
