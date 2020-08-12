3dd9d8c36b46c2de6a70fd9c615c3382121af410
gmanriquev <gmanrique.422@gmail.com>
2020-08-11 22:03:25 -0500

    Bloque 03 parte 02

--- BLOQUE-03/Parte02.sql
+++ BLOQUE-03/Parte02.sql
@@ -33,3 +33,12 @@ inner join Zona z on c.codzona=z.codzona
 where t.estado=1 and c.tipo='E'--teléfonos en estado 'activo' de los clientes empresa
 order by c.codcliente asc
 
+--03.08
+select getdate()--Fecha y Hora del servidor.
+
+select t.tipo as TIPO,t.numero as NUMERO,isnull(c.nombres+' '+c.ape_paterno+' '+c.ape_materno,'SIN_DETALLE') as CLIENTE,
+isnull(c.email,'SIN DETALLE') as EMAIL,convert(varchar(8),getdate(),112) as FEC_CONSULTA
+from Telefono t
+left join Cliente c on t.codcliente=c.codcliente
+where t.estado=1 and c.tipo='P'--teléfonos en estado 'activo' de los clientes persona
+order by c.email desc
