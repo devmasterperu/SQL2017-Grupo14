0806e45f5ee628260621d5cfb6009e3b08256f33
gmanriquev <gmanrique.422@gmail.com>
2020-08-18 20:04:39 -0500

    Bloque 03 parte 03

--- BLOQUE-03/Parte03.sql
+++ BLOQUE-03/Parte03.sql
@@ -14,3 +14,18 @@ from Telefono t
 left join Cliente c on t.codcliente=c.codcliente
 where t.estado=1--teléfonos en estado 'activo' de todos los clientes
 order by c.email desc
+
+--03.09
+--Identificar tablas: Zona, Ubigeo
+--Identificar campo de cruce: codubigeo
+--Identiticar tipo de join: LEFT JOIN
+--Dar formato a las columnas
+
+select z.nombre as ZONA,isnull(u.codubigeo,0) as CODUBIGEO,
+isnull(cod_dpto+cod_prov+cod_dpto,'SIN DATO') as UBIGEO,
+isnull(nombre_dpto,'SIN DATO') as DPTO,
+isnull(nombre_prov,'SIN DATO') as PROV,
+isnull(nombre_dto,'SIN DATO') as DTO
+from Zona z left join Ubigeo u on z.codubigeo=u.codubigeo
+where z.estado=1--zonas en estado 'activo'
+order by z.codzona desc
