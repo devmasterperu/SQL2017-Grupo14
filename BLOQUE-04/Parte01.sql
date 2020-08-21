21e9adf8bfe8409e88ac2b2d8995fd0ed06b682c
gmanriquev <gmanrique.422@gmail.com>
2020-08-20 19:40:05 -0500

    Bloque 04 parte 01

--- BLOQUE-04/Parte01.sql
+++ BLOQUE-04/Parte01.sql
@@ -30,6 +30,20 @@ insert into PlanInternet(descripcion,preciorefsol,nombre) values ('Solicitado po
 --Agregar a los planes la columna fechoraregistro con valor predeterminado de Fecha y hora actual 
 alter table PlanInternet add fechoraregistro datetime default getdate()
 
+begin tran --IMPORTANTE
+delete from PlanInternet where codplan in (13)
+rollback   --SIEMPRE_USAR
+
+DBCC CHECKIDENT('PlanInternet',RESEED,11) --RESETEAR VALOR AUTOGENERADO 11
+
 insert into PlanInternet(descripcion,preciorefsol,nombre) values ('Solicitado por comité junio 2020.',200.00,'STAR II')
+
+select * from PlanInternet
+
+--f
+alter table PlanInternet add estado bit default 0
+
+insert into PlanInternet(descripcion,preciorefsol,nombre,fechoraregistro) 
+values ('Solicitado por comité junio 2020.',210.00,'STAR III','2020-08-20 19:37:00.000')
 
 select * from PlanInternet
